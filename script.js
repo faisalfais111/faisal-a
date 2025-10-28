d28840d2042ab3845f989d039ff23a959a5e28e7
faisalfais111 <faisalfais49312@gmail.com>
2025-10-28 11:37:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 1.pilih tombol berdasarkan ID-nya
-const tomboISapaa=document.getElementByld('sapaButton')
+const tomboISapaa=document.getElementByld('sapaButton');
 // 2. tambah 'even listener' untuk aksi 'click'
 tombolSapa.addEventiListener('click', function(){
-// 3.jalankan kode inni ketika tombol di-klik
-alert('Halo! Terima Kasih sudah berkunjung!');
+// 3.jalankan kode ini ketika tombol di-klik
+alert('Halo! Terima kasih sudah berkunjung!');
 });
